2534c30fa74c92fdde46995d011ddda411cc3545
sqz <vanakaravaj@gmail.com>
2024-08-12 21:18:41 +0300

    Скопировал ветку Ivan в ветку develop, для пользования другим участникам :3

---
+++ FishLegends/map.h
@@ -0,0 +1,17 @@
+#ifndef MAP
+#define MAP
+
+#include <iostream>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+#include <list>
+
+
+using namespace std;
+using namespace sf;
+
+const int HEIGHT_MAP = 20;//размер карты высота
+const int WIDTH_MAP = 26;//размер карты ширина
+
+
+#endif // MAP
